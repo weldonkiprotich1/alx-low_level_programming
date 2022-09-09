c117b493e90542f1f6e445cac9a74968f33db6d7
WELDON CHERUIYOT <weldonkiprotich1@gmail.com>
2022-09-09 12:34:53 -0700

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,7 @@
 nclude <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
  *
  *  * main - Prints a random number and states whether
@@ -15,6 +16,7 @@ nclude <stdlib.h>
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
