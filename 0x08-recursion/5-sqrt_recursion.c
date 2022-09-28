3553e191e75679e09bc14d215919e3ac0b8f7881
WELDON CHERUIYOT <weldonkiprotich1@gmail.com>
2022-09-28 07:32:35 -0700

    Task 05

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * function that returns the natural square root of a number
+ * n does not have a natural square root, the function should return -1
+ *  @a: The entry is equal to n, of the before function
+ *  @b: This is the sum
+ */
+int sqrt_recursion(int a, int b)
+{
+	if (a == (b * b))
+		return (b);
+	else if ((b * b) >= a)
+		return (-1);
+	else
+		return (my_sqrt_recursion(a, b + 1));
+}
+/**
+ * This is the function that is going to perform square root
+ * @n: This is my value
+ */
+int _sqrt_recursion(int n)
+{
+	if (n <= 0)
+		return (-1);
+	return (sqrt_recursion(n, 0));
+}
