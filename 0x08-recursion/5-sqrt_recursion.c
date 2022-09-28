301ad675c382879de6ea8d2f3b83c9e41712c2f3
WELDON CHERUIYOT <weldonkiprotich1@gmail.com>
2022-09-28 07:42:33 -0700

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,6 +4,7 @@
  * n does not have a natural square root, the function should return -1
  *  @a: The entry is equal to n, of the before function
  *  @b: This is the sum
+ *  Return: This is the result
  */
 int sqrt_recursion(int a, int b)
 {
@@ -17,6 +18,7 @@ int sqrt_recursion(int a, int b)
 /**
  * This is the function that is going to perform square root
  * @n: This is my value
+ * eturn: This is my result of the function sqrt_recursion
  */
 int _sqrt_recursion(int n)
 {
