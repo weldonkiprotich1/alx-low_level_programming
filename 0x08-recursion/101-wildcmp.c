ef01c0f90fb129f7522f3027b7262170de7930ce
WELDON CHERUIYOT <weldonkiprotich1@gmail.com>
2022-09-28 08:52:20 -0700

    task 8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -7,29 +7,11 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (!(*s1) && (!(*s2)))  /*Starting point*/
-	{
-		return (1);
-	}
-	else if (*s1 == *s2)
-	{
-		return (wildcmp(s1 + 1, s2 + 1));
-	}
-	if (*s2 == '*')
-	{
-		if (wildcmp(s1, s2 + 1))
-		{
-			return (1);
-		}
-		if (!(*s1))
-		{
-			return (0);
-		}
-		if (wildcmp(s1 + 1, s2))
-		{
-			return (1);
-		}
-	}
-	return (0);2
+	if(*s2 == '\0')
+		return (*s1 == '\0');
+	if(*s2 == *s1)
+		return (*s1 == '\0' && wildcmp(s1 + 1, s2 + 1));
+	if(*s2 == '*')
+		return (wildcmp(s1,s2 + 1) ||(*s2 != '\0' && wildcmp(s1 + 1,s2)));
+	return (0);
 }
-				   
